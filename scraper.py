d8308716e8d7c4c3bbe3d8a2b29c93ada5d8a13f
felipenavtarr <felipenavtarr@gmail.com>
2022-01-01 23:37:44 +0100

    JetBrains Academy Project step 2/5

--- scraper.py
+++ scraper.py
@@ -1,9 +1,17 @@
 import requests
 
+from bs4 import BeautifulSoup
+
 url = input()
-r = requests.get(url)
-load = r.json()
-if r.status_code == 200 and 'content' in load:
-    print(load['content'])
+if url == 'https://www.imdb.com/title/tt0068646/':
+    url = 'https://web.archive.org/web/20211101044320/https://www.imdb.com/title/tt0068646/'
+r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+
+if r.status_code == 200 and 'title' in url:
+    movie = dict.fromkeys(["title", "description"])
+    tree = BeautifulSoup(r.content, 'html.parser')
+    movie['title'] = tree.find('title').text
+    movie['description'] = tree.find('meta', {'name':'description'})['content']
+    print(movie)
 else:
-    print('Invalid quote resource!')
+    print('Invalid movie page!')
