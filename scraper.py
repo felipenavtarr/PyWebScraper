26fe8a78c67fc6c449a1ce83d482a99b172e7e42
felipenavtarr <felipenavtarr@gmail.com>
2022-01-06 19:51:55 +0100

    JetBrains Academy Project step 3/5

--- scraper.py
+++ scraper.py
@@ -1,17 +1,12 @@
 import requests
 
-from bs4 import BeautifulSoup
-
 url = input()
-if url == 'https://www.imdb.com/title/tt0068646/':
-    url = 'https://web.archive.org/web/20211101044320/https://www.imdb.com/title/tt0068646/'
-r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+file = open('source.html', 'wb')
+r = requests.get(url)
 
-if r.status_code == 200 and 'title' in url:
-    movie = dict.fromkeys(["title", "description"])
-    tree = BeautifulSoup(r.content, 'html.parser')
-    movie['title'] = tree.find('title').text
-    movie['description'] = tree.find('meta', {'name':'description'})['content']
-    print(movie)
+if r.status_code == 200:
+    file.write(r.content)
+    file.close()
+    print('Content saved')
 else:
-    print('Invalid movie page!')
+    print(f'The URL returned {r.status_code}')
