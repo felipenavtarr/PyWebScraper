008405a585d04335c3f07bac16579ca80d5dbea5
felipenavtarr <felipenavtarr@gmail.com>
2022-01-17 23:10:06 +0100

    JetBrains Academy Project step 5/5

--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@ import requests
 
 from bs4 import BeautifulSoup
 
+import os
+
 from string import punctuation
 
 from re import sub
@@ -15,25 +17,34 @@ def make_file_name(name):
 
 
 base_url = 'https://www.nature.com'
-url = base_url + '/nature/articles?sort=PubDate&year=2020&page=3'
 saved_articles = []
 
-r = requests.get(url)
+pages = int(input())
+article_type = input()
 
-if r:
-    tree = BeautifulSoup(r.content, 'html.parser')
+for num_page in range(1, pages + 1):
+    url = base_url + f'/nature/articles?sort=PubDate&year=2020&page={str(num_page)}'
+    r = requests.get(url)
 
-    for item in tree.find_all('article'):
-        if item.find('span', class_='c-meta__type').text == 'News':
-            a_tag = item.find('a')
-            file_name = make_file_name(a_tag.text.strip())
-            article_page_tree = BeautifulSoup(requests.get(base_url + a_tag['href']).content, 'html.parser')
-            article_body = article_page_tree.find('div', class_='c-article-body')
-            file = open(file_name, 'wb')
-            file.write(article_body.text.strip().encode('utf-8'))
-            file.close()
-            saved_articles.append(file_name)
+    if r:
+        dir_name = f'Page_{str(num_page)}'
+        os.mkdir(dir_name)
 
-    print('Saved articles:', saved_articles, sep='\n')
-else:
-    print(f'Error in request; status code: {r.status_code}')
+        tree = BeautifulSoup(r.content, 'html.parser')
+
+        for item in tree.find_all('article'):
+            if item.find('span', class_='c-meta__type').text == article_type:
+                a_tag = item.find('a')
+                article_page_tree = BeautifulSoup(requests.get(base_url + a_tag['href']).content, 'html.parser')
+                article_body = article_page_tree.find('div', class_='c-article-body')
+
+                file_name = make_file_name(a_tag.text.strip())
+                file = open(os.path.join(dir_name, file_name), 'wb')
+                file.write(article_body.text.strip().encode('utf-8'))
+                file.close()
+
+                saved_articles.append(file_name)
+    else:
+        print(f'Error in request; status code: {r.status_code}')
+
+print('Saved articles:', saved_articles, sep='\n')
